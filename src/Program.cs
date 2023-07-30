6cff52e40ae0bf14956d963c044ab721643d3f84
Denys Volodarskyi <denys@volodarskyi.com>
2023-07-30 16:48:31 +0300

    Source generator to create format list

--- src/Program.cs
+++ src/Program.cs
@@ -1,62 +1,72 @@
 ﻿using BinFmtScan;
-using BinFmtScan.Formats.Images;
 
-var path = Environment.GetCommandLineArgs()[1];
-var out_dir = path + " extracted";
-
-using (var stream = File.OpenRead(path))
+internal class Program
 {
-    var src = new BinarySource(stream);
-    Scan(src);
-}
-
-return;
-
-void Scan(BinarySource src)
-{
-    while (src.Position < src.Length)
+    private static void Main(string[] args)
     {
-        var pos = src.Position;
-        FoundInfo? fmt = null;
+        if (args.Length == 0)
+            return;
 
-        IDetector? det = null;
-        FindAnyFormat(src, ref fmt, ref det);
+        var path = args[0];
+        if (string.IsNullOrEmpty(path) | !File.Exists(path))
+            return;
 
-        if (det != null && fmt != null && fmt.HasSize)
-        {
-            Directory.CreateDirectory(out_dir);
-            var fn = Path.Combine(out_dir, $"{fmt.StartPosition:X}{det.Extension}");
-            src.Dump(fmt.StartPosition, fmt.Size, fn);
-        }
+        var out_dir = path + " extracted";
 
-        src.Position = fmt != null && fmt.HasSize ? pos + fmt.Size : pos + 1;
+        using var stream = File.OpenRead(path);
+        var src = new BinarySource(stream);
+        Scan(src, out_dir);
     }
-}
 
-static void FindAnyFormat(BinarySource src, ref FoundInfo? fmt, ref IDetector? detector)
-{
-    foreach (var handler in FormatList.All)
+    private static void FindAnyFormat(BinarySource src, ref FoundInfo? fmt, ref IDetector? detector)
     {
-        try
+        foreach (var handler in FormatList.All)
         {
-            handler.Detect(src, ref fmt);
+            try
+            {
+                handler.Detect(src, ref fmt);
+            }
+            catch
+            {
+            }
+
+            if (fmt != null)
+            {
+                // Format found.
+                detector = handler;
+
+                Console.Write($"0x{fmt.StartPosition:X}");
+                if (fmt.HasSize)
+                    Console.Write($"..0x{fmt.StartPosition + fmt.Size:X}");
+                Console.Write($": {handler.ID}");
+                Console.WriteLine();
+
+                break;
+            }
         }
-        catch
+    }
+
+    private static void Scan(BinarySource src, string out_dir, bool dump = false)
+    {
+        while (src.Position < src.Length)
         {
-        }
+            var pos = src.Position;
+            FoundInfo? fmt = null;
 
-        if (fmt != null)
-        {
-            // Format found.
-            detector = handler;
+            IDetector? det = null;
+            FindAnyFormat(src, ref fmt, ref det);
 
-            Console.Write($"0x{fmt.StartPosition:X}");
-            if (fmt.HasSize)
-                Console.Write($"..0x{(fmt.StartPosition + fmt.Size):X}");
-            Console.Write($": {handler.ID}");
-            Console.WriteLine();
+            if (dump)
+            {
+                if (det != null && fmt != null && fmt.HasSize)
+                {
+                    Directory.CreateDirectory(out_dir);
+                    var fn = Path.Combine(out_dir, $"{fmt.StartPosition:X}{det.Extension}");
+                    src.Dump(fmt.StartPosition, fmt.Size, fn);
+                }
+            }
 
-            break;
+            src.Position = fmt != null && fmt.HasSize ? pos + fmt.Size : pos + 1;
         }
     }
 }
