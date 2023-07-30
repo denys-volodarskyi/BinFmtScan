18086bb77f3192b7920b67cdda4cb36c62bbdf45
Denys Volodarskyi <denys@volodarskyi.com>
2023-07-30 19:01:23 +0300

    Cmd line args, show usage

--- src/Program.cs
+++ src/Program.cs
@@ -5,17 +5,79 @@ internal class Program
     private static void Main(string[] args)
     {
         if (args.Length == 0)
+        {
+            ShowUsage();
             return;
+        }
 
-        var path = args[0];
+
+        bool extract = false;
+        string path = "";
+
+        for (var i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "-e":
+                    extract = true;
+                    break;
+            }
+
+            if (i == args.Length - 1)
+                path = args[i];
+            else if (args[i] == "-x")
+                extract = true;
+            else
+                Console.WriteLine($"Unknown switch: {args[i]}");
+        }
+
+
         if (string.IsNullOrEmpty(path) | !File.Exists(path))
+        {
+            Console.WriteLine("Invalid source path");
             return;
+        }
 
         var out_dir = path + " extracted";
 
         using var stream = File.OpenRead(path);
         var src = new BinarySource(stream);
-        Scan(src, out_dir);
+        Scan(src, out_dir, extract);
+    }
+
+    private static void Scan(BinarySource src, string out_dir, bool extract = false)
+    {
+        int count = 0;
+
+        while (src.Position < src.Length)
+        {
+            var pos = src.Position;
+            FoundInfo? fmt = null;
+
+            IDetector? det = null;
+            FindAnyFormat(src, ref fmt, ref det);
+
+            if (fmt != null)
+            {
+                count++;
+
+                if (det != null && fmt.HasSize)
+                {
+                    if (extract)
+                    {
+                        Directory.CreateDirectory(out_dir);
+                        var fn = Path.Combine(out_dir, $"{fmt.StartPosition:X}{det.Extension}");
+                        src.Dump(fmt.StartPosition, fmt.Size, fn);
+                    }
+                }
+            }
+
+            src.Position = fmt != null && fmt.HasSize ? pos + fmt.Size : pos + 1;
+        }
+
+
+        if (count == 0)
+            Console.WriteLine("Nothing found");
     }
 
     private static void FindAnyFormat(BinarySource src, ref FoundInfo? fmt, ref IDetector? detector)
@@ -46,27 +108,29 @@ internal class Program
         }
     }
 
-    private static void Scan(BinarySource src, string out_dir, bool dump = false)
+    private static void ShowUsage()
     {
-        while (src.Position < src.Length)
-        {
-            var pos = src.Position;
-            FoundInfo? fmt = null;
+        var fg = Console.ForegroundColor;
 
-            IDetector? det = null;
-            FindAnyFormat(src, ref fmt, ref det);
+        Console.WriteLine();
 
-            if (dump)
-            {
-                if (det != null && fmt != null && fmt.HasSize)
-                {
-                    Directory.CreateDirectory(out_dir);
-                    var fn = Path.Combine(out_dir, $"{fmt.StartPosition:X}{det.Extension}");
-                    src.Dump(fmt.StartPosition, fmt.Size, fn);
-                }
-            }
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("Binary Format Scanner");
+        Console.ForegroundColor = fg;
 
-            src.Position = fmt != null && fmt.HasSize ? pos + fmt.Size : pos + 1;
-        }
+        Console.WriteLine(" by Denys Volodarskyi");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("https://github.com/denys-volodarskyi/BinFmtScan");
+        Console.ForegroundColor = fg;
+
+        Console.WriteLine();
+        Console.WriteLine(
+            $"""
+            Usage: {AppDomain.CurrentDomain.FriendlyName} [options] filename
+
+            Options:
+              -x: extract
+            """);
     }
 }
